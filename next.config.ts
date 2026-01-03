444ab9c129c8c17cf8c93bd9ad55b4718d625bb3
Jame <monton.onnom@gmail.com>
2026-01-03 17:35:36 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
+    removeConsole: process.env.NODE_ENV === "production",
   },
   images: {
     remotePatterns: [
